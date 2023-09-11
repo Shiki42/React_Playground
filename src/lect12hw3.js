bb4c383c1772efaa39cb1a5418e9017ed013246e
shuyuan hu <kukutou_mikiya@outlook.com>
2023-09-11 06:40:29 +0000

    lect12 hw

--- src/lect12hw3.js
+++ src/lect12hw3.js
@@ -1,16 +1,39 @@
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div>
-    <h2>What is Frontend?</h2>
-    <ul>
-      <li>
-        Frontend is the part of the website that users can see and interact with.
-      </li>
-      <li>Frontend is also called <strong>client-side</strong>.</li>
-      <li>Frontend is built with HTML, CSS, and JavaScript.</li>
-    </ul>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleClick1 = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  };
+
+  handleClick10 = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 10 }));
+  };
+
+  handleClick100 = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 100 }));
+  };
+
+  render() {
+    return (
+      <div>
+      <div>
+        <p>counter: {this.state.counter}</p>
+      </div>
+      <button onClick = {this.handleClick1}> +1 </button>
+      <button onClick = {this.handleClick10}> +10 </button>
+      <button onClick = {this.handleClick100}> +100 </button>
+      </div>
+    );
+  }
 }
+
+export default App;
---
+++ src/lect12hw4.js
@@ -0,0 +1,51 @@
+import "./styles.css";
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:'',
+      trans:''
+    };
+  }
+  
+  transfer = (e) => {
+    let _int = parseInt(e);
+    let _pst = '';
+    if (_int) {
+        if ( _int % 10 === 1 ){
+            _pst = 'st';
+        }
+        else if ( _int % 10 === 2 ) {
+            _pst = 'nd';
+        }
+        else if ( _int % 10 === 3 ) {
+            _pst = 'rd';
+        }
+        
+        return e + _pst;
+    }
+    else {
+        return e;
+    }
+  }
+
+  handleClick = (e) => {
+    this.setState(prevState => ({ text: e.target.value }));
+    this.state.trans=this.transfer(e.target.value);
+  };
+
+
+  render() {
+    return (
+      <div>
+      <input onChange = {this.handleClick} />
+    <div>{this.state.trans}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
