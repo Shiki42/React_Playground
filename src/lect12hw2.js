8a6c34b01750efad6f046becf9f811a21ec2fef0
shuyuan hu <kukutou_mikiya@outlook.com>
2023-09-11 06:02:59 +0000

    Update 7 files

---
+++ src/lect12hw2.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+<body>
+    <header>
+        <h1>Your Website Header</h1>
+    </header>
+    <nav>
+        <ul>
+            <div> header </div>
+        </ul>
+    </nav>
+    <aside>
+        <h2>Aside</h2>
+    </aside>
+    <section>
+        <h2>Section</h2>
+    </section>
+    <footer>
+        <p>Footer</p>
+    </footer>
+</body>
+  );
+}
---
+++ src/lect12hw3.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+    <h2>What is Frontend?</h2>
+    <ul>
+      <li>
+        Frontend is the part of the website that users can see and interact with.
+      </li>
+      <li>Frontend is also called <strong>client-side</strong>.</li>
+      <li>Frontend is built with HTML, CSS, and JavaScript.</li>
+    </ul>
+    </div>
+  );
+}
